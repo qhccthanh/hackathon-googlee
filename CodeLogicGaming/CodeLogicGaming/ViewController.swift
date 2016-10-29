122dfe0f34eb15e50a125803573d0eaa4b790524
Bao Bao <nguyenbao.95@gmail.com>
2016-10-29 09:22:24 +0700

    Code logic

---
+++ CodeLogicGaming/CodeLogicGaming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeLogicGaming
+//
+//  Created by MACMALL MF840 on 10/29/16.
+//  Copyright © 2016 Googlee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
