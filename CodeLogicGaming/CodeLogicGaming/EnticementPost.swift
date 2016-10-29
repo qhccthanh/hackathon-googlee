efdcda5fe85d96f9fdbb0ef0fb902efdaa855d4e
Bao Bao <nguyenbao.95@gmail.com>
2016-10-29 11:10:44 +0700

    updateeeee

--- CodeLogicGaming/CodeLogicGaming/EnticementPost.swift
+++ CodeLogicGaming/CodeLogicGaming/EnticementPost.swift
@@ -21,10 +21,23 @@ class EnticementPost: NSObject {
     // Properties
     var host: UserAccount = UserAccount()
     var postTime: Double = 0.0 // Time interval since 1970
-    var content: Content = ""
+    var content: String = ""
     var categories: Array<Category> = Array()
     var hostLocation: Double = 0.0
     var interestedList: Array<UserAccount> = Array() // Nhưng người có hứng thú vs post này
     var joinedList: Array<UserAccount> = Array() // Danh sách người đã đăng ký
     
+    let kHostKey = "hostID"
+    let kPostTimeKey = "postTime"
+    let kContentKey = "content"
+    let categoriesKey = "categories"
+    
+    
+    override init() {
+        super.init()
+    }
+    
+    init(withDictionary data: NSDictionary) {
+        super.init()
+    }
 }
